2806fe44ac4560630c4159a43f7b2c9be19c9160
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2021-07-22 21:51:29 +0800

    Add files via upload

---
+++ day-37-habit-tracker/main.py
@@ -0,0 +1,71 @@
+import requests
+from datetime import datetime
+
+USERNAME = "rachana99"
+TOKEN = ""  # Keep this secure
+GRAPH_ID = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Coding Graph",
+    "unit": "minute",
+    "type": "int",
+    "color": "momiji"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "135"
+}
+
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+# print(response.text)
+
+yesterday = datetime(year=2021, month=7, day=21)
+pixel_update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{yesterday.strftime('%Y%m%d')}"
+
+pixel_update_data = {
+    "quantity": "328",
+}
+
+# response = requests.put(url=pixel_update_endpoint, json=pixel_update_data, headers=headers)
+# print(response.text)
+
+pixel_delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+# response = requests.delete(url=pixel_delete_endpoint, headers=headers)
+# print(response.text)
+
+graph_update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+graph_update_data = {
+    "color": "ajisai",
+}
+# response = requests.put(url=graph_update_endpoint, json=graph_update_data, headers=headers)
+# print(response.text)
+
+
