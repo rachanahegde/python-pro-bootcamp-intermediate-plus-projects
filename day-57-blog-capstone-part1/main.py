649b23fa6c48709ee2a582bdd0e3530fc3415b3c
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2021-12-18 13:49:34 +0000

    Add files via upload

--- day-57-blog-capstone-part1/main.py
+++ day-57-blog-capstone-part1/main.py
@@ -1,21 +1,28 @@
 from flask import Flask, render_template
 import requests
 
+response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+all_posts = response.json()
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
-    all_posts = response.json()
     return render_template("index.html", posts=all_posts)
 
 
-app.route('/post/<int:blog_id>')
-def get_post(blog_id):
-    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
-    all_posts = response.json()
-    return render_template("post.html", posts=all_posts, blog_id=blog_id)
+@app.route("/post/<int:index>")
+def show_post(index):
+    title = None
+    subtitle = None
+    body = None
+    for post in all_posts:
+        if post['id'] == index:
+            title = post['title']
+            subtitle = post['subtitle']
+            body = post['body']
+    return render_template("post.html", title=title, subtitle=subtitle, body=body)
 
 
 if __name__ == "__main__":
